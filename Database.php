4050bc89b1cc77bf1a8f21b3fec3ba06c2da4dee
Patryk Persak <abogusiac@gmail.com>
2025-12-21 17:04:55 +0100

    Add Database connection

---
+++ Database.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once "config.php";
+
+class Database {
+    private $username;
+    private $password;
+    private $host;
+    private $database;
+
+    public function __construct()
+    {
+        $this->username = USERNAME;
+        $this->password = PASSWORD;
+        $this->host = HOST;
+        $this->database = DATABASE;
+    }
+
+    public function connect()
+    {
+        try {
+            $conn = new PDO(
+                "pgsql:host=$this->host;port=5432;dbname=$this->database",
+                $this->username,
+                $this->password,
+                [
+                    "sslmode" => "prefer",
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                ]
+            );
+
+            return $conn;
+        }
+        catch(PDOException $e) {
+            error_log("Database connection failed: " . $e->getMessage());
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+}
